a98ca10cd7c5c07a73f7e6434491d3fc99020faa
Vinitbagalkoti <vinitbagalkoti00@gmail.com>
2021-06-15 19:20:36 +0530

    Attendence check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,17 @@
 
 echo "Welcome to the empcoputation"
 
+# constant
+IS_PRESENT=1
+
+
+#VARIABLE
+empCheck=$(( RANDOM % 2 ))
+
+if [ $empCheck -eq $IS_PRESENT ]
+then
+	echo "emp is p"
+else 
+	echo "emp is ab"
+fi
+
